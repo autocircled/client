a2ced885e498f4ed3c5ae0ca874b3ff4047d2cf3
Moktadir Rahman <autocircled@gmail.com>
2023-07-18 16:45:19 +0600

    register page, formRow and alert -ok

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,68 @@
+import React, { useState } from 'react';
+import Wrapper from '../assets/wrappers/RegisterPage';
+import { Logo, FormRow, Alert } from '../components';
+
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    isMember: true,
+    showAlert: false,
+}
 const Register = () => {
+    const [values, setValues] = useState(initialState);
+
+    const handleChange = (e) => {
+        console.log(e.target);
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        })
+    }
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+    }
+
     return (
-        <div>
-            <h1>Register</h1>
-        </div>
+        <Wrapper className="full-page">
+            <Logo />
+            <form className="form" onSubmit={onSubmit}>
+                <h3>Register</h3>
+
+                {/* Alert */}
+                {values.showAlert && <Alert />}
+
+                {/* Name Input */}
+                <FormRow
+                    type='text'
+                    name="name"
+                    value={values.name}
+                    handleChange={handleChange}
+                />
+
+                {/* Email Input */}
+                <FormRow
+                    type='text'
+                    name="email"
+                    value={values.email}
+                    handleChange={handleChange}
+                />
+
+                {/* Password Input */}
+                <FormRow
+                    type='password'
+                    name="password"
+                    value={values.password}
+                    handleChange={handleChange}
+                />
+
+                <button type="submit" className="btn btn-block">
+                    submit
+                </button>
+            </form>
+
+        </Wrapper>
     )
 }
 
